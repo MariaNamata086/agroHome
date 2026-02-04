3830279d6d6ce1ea602c1eb468d7a3515029adeb
Maria Namata <namatamaria086@gmail.com>
2026-02-04 21:29:27 +0300

    Remove the delay in home page header section transitions

--- app/components/page-specific/home/HeroSection.tsx
+++ app/components/page-specific/home/HeroSection.tsx
@@ -25,7 +25,7 @@ const HeroSection = () => {
       >
         <div className="relative z-20 h-full flex flex-col md:mt-20 w-full md:w-[55%] gap-4 md:gap-14 tracking-wider leading-loose">
           <div
-            className={`flex gap-3 w-1/2  items-center  ${inView ? 'translate-y-8 opacity-100' : '-translate-y-7 opacity-0'} transform transition-transform ease-in duration-1500 delay-50`}
+            className={`flex gap-3 w-1/2  items-center  ${inView ? 'translate-y-8 opacity-100' : '-translate-y-7 opacity-0'} transform transition-transform ease-in duration-1500 delay-0`}
           >
             <GiThreeLeaves color="#96CA2D" size={30} />
             <span className="flex text-[12px] md:text-[15px] font-gochi text-forestGreen tracking-widest">
@@ -33,12 +33,12 @@ const HeroSection = () => {
             </span>
           </div>
           <h1
-            className={`font-bold text-xl md:text-5xl text-forestGreen leading-relaxed ${inView ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'} transform transition-transform ease-in-out duration-1500 delay-50`}
+            className={`font-bold text-xl md:text-5xl text-forestGreen leading-relaxed ${inView ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'} transform transition-transform ease-in-out duration-1500 delay-0`}
           >
             Growing Food. Training Farmers, Building Legacies
           </h1>
           <p
-            className={`text-forestGreen text-md md:text-lg ${inView ? '-translate-y-5 opacity-100' : 'translate-y-6 opacity-0'} transform transition-transform ease-in duration-1500 delay-50`}
+            className={`text-forestGreen text-md md:text-lg ${inView ? '-translate-y-5 opacity-100' : 'translate-y-6 opacity-0'} transform transition-transform ease-in duration-1500 delay-0`}
           >
             A model farm and training center in Luweero District, Uganda &mdash;
             transforming agricluture through innovation, integrity, and impact.
