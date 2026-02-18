23bc3fc074055d94713f7093a52a765155502d50
Maria Namata <namatamaria086@gmail.com>
2026-02-18 19:14:35 +0300

    Further increase the height of the bananas image on the AbouUsBreif component to slightly overlap the poultry cages image

--- app/components/page-specific/home/AboutUsBrief.tsx
+++ app/components/page-specific/home/AboutUsBrief.tsx
@@ -28,7 +28,7 @@ const AboutUsBrief = () => {
             <Image
               src={bananaProjectPicture}
               alt="a picture of a banana farm project"
-              className="mt-10 w-1/2 h-80 object-cover"
+              className="mt-10 w-1/2 h-100 object-cover"
             />
           </div>
         </Motion>
