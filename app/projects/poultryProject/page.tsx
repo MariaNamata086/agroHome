5f0df22346624a1467c3c0b25c7eb4f235e08dbb
Maria Namata <namatamaria086@gmail.com>
2026-02-19 19:48:08 +0300

    Improve mobile responsiveness of poultry project page

--- app/projects/poultryProject/page.tsx
+++ app/projects/poultryProject/page.tsx
@@ -102,22 +102,22 @@ function PoultryProject() {
         'High-quality day-old chicks, controlled brooding and high grade early nutrition.',
     },
     {
-      heading: 'Vaccination & Health Management',
+      heading: 'Health Management',
       moreInfo:
-        'Strict vaccination schedules and biosecurity protocols to ensure bird health.',
+        'Strict vaccination schedules,immune boosting regimens and proper hygiene to ensure bird health.',
     },
     {
-      heading: 'Deep Litter Rearing (Up to 3.5 Months)',
+      heading: 'Deep Litter Rearing',
       moreInfo:
-        'Optimal growth through proper space, feeding, and sanitation practices.',
+        'Optimal growth through proper space, feeding, and sanitation practices, Up to 3.5 Months.',
     },
     {
-      heading: 'Transition to Battery Cage System',
+      heading: 'Battery Cage System',
       moreInfo:
         'Birds are moved to larger cages as they attain egg laying age, with efficient feeding programs and improved hygiene.',
     },
     {
-      heading: 'Feed Processing & Egg Production',
+      heading: 'Feed Processing',
       moreInfo:
         'On-site feed milling and contentrate reconstitution using broken maize and quality control systems.',
     },
@@ -134,15 +134,15 @@ function PoultryProject() {
     <main className="flex flex-col gap-10">
       <HeaderBanner
         page="Poultry Project"
-        pageHeading="From chick to crate - a complete poultry production journey . "
+        pageHeading="From chick to crate - a complete poultry production journey "
         src={poultryFarmingHero}
       />
 
-      <div className="flex flex-col bg-white gap-20 justify-center items-center p-10 md:p-20">
-        <div className="flex flex-col w-full md:w-[80%] h-auto">
+      <div className="flex flex-col bg-white gap-20 justify-center items-center md:p-20">
+        <div className="flex flex-col w-full md:w-[80%] h-auto p-8">
           <Motion direction="right">
             <div className="flex flex-col gap-6 -mt-14 md:text-5xl ">
-              <h1 className="flex items-center tracking-widest p-3 md:p-5 text-slate-800 justify-center font-bold text-[35px] md:text-5xl md:gap-6 bg-amber200 text-center">
+              <h1 className="flex items-center tracking-widest p-3 md:p-5 text-slate-800 justify-center font-bold text-[35px] md:text-5xl gap-2 md:gap-6 bg-amber200 text-center">
                 <FontAwesomeIcon
                   icon={faBinoculars}
                   size="xl"
@@ -151,7 +151,7 @@ function PoultryProject() {
                 />
                 <span> Project Overview</span>
               </h1>
-              <p className=" tracking-widest text-justify text-slate-700 leading-8 md:leading-loose text-sm md:text-[17px]">
+              <p className=" tracking-widest text-justify text-slate-700 leading-8 md:leading-loose text-[16px] md:text-[18px]">
                 {' '}
                 Our poultry project is the backbone of the farm. We specialize
                 primarily in <b>layer birds,</b>reared using a{' '}
@@ -159,31 +159,31 @@ function PoultryProject() {
                 that ensures efficiency, top-notch bird welfare and superior egg
                 quality.
               </p>
-              <p className="tracking-widest text-slate-700 text-justify leading-8 md:leading-loose text-sm md:text-[17px]">
+              <p className="tracking-widest text-slate-700 text-justify leading-8 md:leading-loose text-[16px] md:text-[18px]">
                 This system allows us to manage over <b>10,000 birds</b> with
                 precision, consistency, and strict hygiene standards
               </p>
-              <p className=" tracking-widest text-slate-700 text-justify leading-8 md:leading-loose text-sm md:text-[17px]">
-                Our layer enterprise serves both as a commercial operation and a
-                training model for farmers seeking to transition to efficient,
-                hygienic and profitable poultry production.
+              <p className=" tracking-widest text-slate-700 text-justify leading-8 md:leading-loose text-[16px] md:text-[18px]">
+                Our layer poultry enterprise serves both as a commercial
+                operation and a training model for farmers seeking to transition
+                to efficient, hygienic and profitable poultry production.
               </p>
             </div>
           </Motion>
         </div>
 
-        <div className="flex relative h-250">
+        <div className="flex relative h-300 md:h-250">
           <Image
             src={poultryStagesImage}
             alt="image showing poultry day old chicks in a brooder"
             style={{ objectFit: 'cover' }}
           />
-          <div className="absolute inset-0 bg-black/50 flex gap-2 flex-col items-center justify-center h-auto p-6 md:p-10 w-full tracking-wider leading-loose">
+          <div className="absolute inset-0 bg-black/60 flex gap-2 flex-col items-center justify-center h-auto p-4 md:p-10 w-full tracking-wider leading-loose">
             <div className="flex flex-col  ">
               <Motion direction="left">
-                <h2 className="flex items-center tracking-widest md:p-5 text-white md:text-slate-200 md:leading-18 justify-center font-bold text-[28px] md:text-5xl gap-2 md:gap-6 bg-amber200 mb-4 text-center">
+                <h2 className="flex items-center tracking-widest md:p-5 text-white md:text-slate-200 font-bold text-[28px] md:text-5xl md:leading-18 justify-center  gap-2 md:gap-6 bg-amber200 mb-4 text-center">
                   <FontAwesomeIcon icon={faRoute} beat size="xl" />
-                  <span>
+                  <span className="font-bold text-[32px] md:text-5xl">
                     {' '}
                     Let's take you through the poultry farming journey
                   </span>
@@ -222,7 +222,7 @@ function PoultryProject() {
                                   ? 'black'
                                   : 'oklch(55.4% 0.046 257.417)',
                             }}
-                            className="text-[12px] font-medium tracking-widest text-center md:text-[15px] text-forestGreen"
+                            className="text-[13px] font-medium tracking-widest text-center md:text-[15px] text-forestGreen"
                           >
                             {heading}
                           </h3>
@@ -234,14 +234,14 @@ function PoultryProject() {
                                 ? 'oklch(30.2% 0.056 229.695)'
                                 : 'oklch(37.2% 0.044 257.287)',
                           }}
-                          className="flex tracking-relaxed text-[11px] md:text-[14px] text-center leading-relaxed mb-2"
+                          className="flex tracking-relaxed text-[12px] md:text-[14px] text-center leading-relaxed mb-2"
                         >
                           {moreInfo}
                         </span>
                       </div>
                     </div>
                     <span
-                      className="flex -mt-8 w-0.5 h-14.25 md:h-20.25 bg-slate-200 ml-6"
+                      className="flex -mt-8 w-0.5 h-17.25 md:h-20.25 bg-slate-200 ml-6"
                       style={{
                         backgroundColor:
                           index === 0 || index === 1 || index === 2
@@ -255,11 +255,11 @@ function PoultryProject() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-3 md:gap-10 bg-gray-100 p-6 md:p-10 rounded-b-xl tracking-wider leading-loose">
+        <div className="flex flex-col gap-3 md:gap-10 bg-gray-100 px-6 md:px-20 rounded-b-xl tracking-wider leading-loose ">
           <Motion direction="right">
-            <h2 className="flex items-center tracking-widest p-3 md:p-5 text-gray-700 justify-center font-bold text-[34px] md:text-5xl gap-3.5">
-              <GiBirdCage className="text-amber-500 text-[80px]" />
-              <span>Why Battery Cage System?</span>
+            <h2 className="flex items-center tracking-widest leading-loose p-3 md:p-5 text-gray-700 justify-center text-center font-bold text-[35px] md:text-5xl gap-3.5">
+              <GiBirdCage className="text-amber-500  text-9xl md:text-[80px]" />
+              <span>Why a Battery Cage System?</span>
             </h2>
           </Motion>
           <div ref={ref} className="grid md:grid-cols-2 gap-6">
@@ -270,7 +270,7 @@ function PoultryProject() {
                     className={`flex flex-col gap-4 py-3 px-4 md:px-6 shadow-lg rounded-md hover:scale-103 duration-1000 ${inView ? 'translate-x-0 opacity-100' : '-translate-x-10 opacity-0'} `}
                     key={index}
                   >
-                    <div className="flex items-center gap-2.5 p-2">
+                    <div className="flex items-center gap-2.5 p-3">
                       <h3 className="font-semibold text-[19px] md:text-xl tracking-widest text-slate-700">
                         {advantage}
                       </h3>
@@ -280,7 +280,7 @@ function PoultryProject() {
                         color="oklch(37.3% 0.034 259.733)"
                       />
                     </div>
-                    <p className="tracking-wider text-[15px] text-right md:text-[17px] leading-loose">
+                    <p className="tracking-wider text-[15px] text-right md:text-[17px] px-2 leading-loose">
                       {moreInfo}
                     </p>
                   </div>
@@ -289,7 +289,7 @@ function PoultryProject() {
             )}
           </div>
         </div>
-        <section className="relative mb-1 w-full h-87.5 md:h-130">
+        <section className="relative mb-1 w-full md:px-20 h-87.5 md:h-130">
           <Image
             src={cleanEggsPicture}
             alt="Image of a poultry house showing battery cages with cloean eggs ready for collection"
@@ -299,7 +299,7 @@ function PoultryProject() {
           />
 
           <div className="absolute inset-0 bg-black/60 flex flex-col items-center justify-center px-5 gap-4 md:gap-10 text-center text-gray-300">
-            <h2 className="font-semibold  text-white text-2xl tracking-widest md:text-4xl">
+            <h2 className="font-semibold  text-white text-2xl tracking-widest md:text-5xl">
               Project Deliverables
             </h2>
             <div
